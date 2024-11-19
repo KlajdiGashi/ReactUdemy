83531850f2f03ce80f1e20dc862673824250e4a5
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-19 10:29:04 +0100

    "import" & "export" keywords and usage

---
+++ Scripts/app.js
@@ -0,0 +1,17 @@
+//import { apiKey } from "./util.js"; 
+
+import apiKey from "./util.js"
+ 
+//import { otherExample, otherExample1 } from "./util.js";
+
+import * as util from "./util.js"
+
+import {apiKey as content} from "./util.js"
+
+console.log(util.apiKey);
+
+console.log(apiKey);
+
+console.log(content);
+//console.log(otherExample);
+//console.log(otherExample1);
