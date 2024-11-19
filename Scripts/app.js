88bae2972d26be6dd759542700beba68c718c55e
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-19 11:14:50 +0100

    Revisiting Operators

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,25 @@
-//import { apiKey } from "./util.js"; 
+let userMessage = "Hello World!";
 
+userMessage = "New Value";
+
+console.log(userMessage);
+console.log(userMessage);
+
+console.log(10 / 5);
+console.log(10 + 5);
+console.log("Hello" + " World"); // Concat
+console.log(10 === 5);
+console.log(10 === 10);
+console.log(10 <= 10);
+
+if (10 === 10) {
+  console.log("Works");
+}
+
+/* 
+//import { apiKey } from "./util.js"; 
 import apiKey from "./util.js"
- 
-//import { otherExample, otherExample1 } from "./util.js";
+ import { otherExample, otherExample1 } from "./util.js";
 
 import * as util from "./util.js"
 
@@ -13,5 +30,7 @@ console.log(util.apiKey);
 console.log(apiKey);
 
 console.log(content);
-//console.log(otherExample);
-//console.log(otherExample1);
+console.log(otherExample);
+console.log(otherExample1);
+
+*/
