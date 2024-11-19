8d6134ac8b850b4a343e328df7010aab902b61df
Klajdi Gashi <klajdi.gashi01@gmail.com>
2024-11-19 12:46:40 +0100

    Revisiting Objects and Classes

--- Scripts/app.js
+++ Scripts/app.js
@@ -21,6 +21,52 @@ function combine(a, b, c) {
 const abc = combine(22, 34, 4);
 console.log(abc);
 
+export default (userName, message) => {
+  return userName + message;
+};
+
+(userName) => {
+  console.log(userName);
+};
+
+(number) => number * 3;
+
+(number) => ({
+  age: number,
+}); // this is the correct way to return an object in javascript.
+
+const userName = "Max";
+const userAge = 34;
+
+const user = {
+  name: "Max",
+  age: 34,
+  greet() {
+    console.log("Hello!");
+    console.log(this.age);
+    console.log(this.name);
+  },
+};
+
+console.log(user);
+console.log(user.age);
+user.greet();
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log("Hi!");
+  }
+}
+
+const user1 = new User("Manuel", 35);
+
+console.log(user1);
+user1.greet();
 /* let userMessage = "Hello World!";
 
 userMessage = "New Value";
